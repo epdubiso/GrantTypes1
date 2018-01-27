27a30cc1816cd2033f4d6859ac81317969207d8b
epdubiso <35310550+epdubiso@users.noreply.github.com>
2018-01-26 16:19:36 -0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017MVC.Models;
+
+namespace CommunityAssist2017MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            //var gt = (from g in db.GrantTypes
+                      //select g).ToList();
+            return View(db.GrantTypes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
